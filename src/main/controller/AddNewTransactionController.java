f617c9bc7b5484ea10db7764ef7dad57d19b8043
lumoyano <121978205+lumoyano@users.noreply.github.com>
2023-08-24 16:58:31 -0700

    Finished controller classes

--- src/main/controller/AddNewTransactionController.java
+++ src/main/controller/AddNewTransactionController.java
@@ -1,5 +1,31 @@
 package main.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import main.model.Transaction;
+import main.serviceData.BlockchainData;
+
+import java.security.GeneralSecurityException;
+import java.security.Signature;
+import java.util.Base64;
+
 public class AddNewTransactionController {
+
+    @FXML
+    private TextField toAddress;
+    @FXML
+    private TextField value;
+
+    @FXML
+    public void createNewTransaction () throws GeneralSecurityException {
+        Base64.Decoder Decoder = Base64.getDecoder();
+        Signature signing = Signature.getInstance("SHA256withDSA");
+        Integer ledgerID = BlockchainData.getInstance().getTransactionLedgerFX().get(0).getLedgerID();
+        byte[] sendB = Decoder.decode(toAddress.getText());
+        Transaction transaction = new Transaction(WalletData.getInstance().getWallet(), sendB,
+                Integer.parseInt(value.getText()), ledgerID, signing);
+        BlockchainData.getInstance().addTransaction(transaction, false);
+        BlockchainData.getInstance().addTransactionState(transaction);
+    }
 
 }
---
+++ src/main/controller/MainWindowController.java
@@ -0,0 +1,86 @@
+package main.controller;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.BorderPane;
+import main.model.Transaction;
+import main.serviceData.BlockchainData;
+
+import javax.swing.text.PlainDocument;
+import javax.swing.text.html.Option;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.Optional;
+
+public class MainWindowController {
+    //this is read-only UI table
+    @FXML
+    public TableView<Transaction> tableView = new TableView<>();
+    @FXML
+    private TableColumn<Transaction, String> from;
+    @FXML
+    private TableColumn<Transaction, String> to;
+    @FXML
+    private TableColumn<Transaction, Integer> value;
+    @FXML
+    private TableColumn<Transaction, String> timestamp;
+    @FXML
+    private TableColumn<Transaction, String> signature;
+    @FXML
+    private BorderPane borderPane;
+    @FXML
+    private TextField eCoins;
+    @FXML
+    private TextArea publicKey;
+
+    public void initialize() {
+        Base64.Encoder encoder = Base64.getEncoder();
+        from.setCellValueFactory(new PropertyValueFactory<>("fromFX"));
+        to.setCellValueFactory(new PropertyValueFactory<>("toFX"));
+        value.setCellValueFactory(new PropertyValueFactory<>("value"));
+        signature.setCellValueFactory(new PropertyValueFactory<>("signatureFX"));
+        timestamp.setCellValueFactory(new PropertyValueFactory<>("timestamp"));
+        eCoins.setText(BlockchainData.getInstance().getWalletBalanceFX("eCoins"));
+        publicKey.setText(encoder.encodeToString(WalletData.getInstance().getWallet().getPublicKey().getEncoded()));
+        tableView.setItems(BlockchainData.getInstance().getTransactionLedgerFX());
+        tableView.getSelectionModel().select(0);
+    }
+
+    @FXML
+    public void toNewTransactionController() {
+        Dialog<ButtonType> newTransactionController = new Dialog<>();
+        newTransactionController.initOwner(borderPane.getScene().getWindow());
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("main/view/AddNewTransactionWindow.fxml"));
+        try {
+            newTransactionController.getDialogPane().setContent(fxmlLoader.load());
+
+        } catch (IOException e) {
+            System.out.println("Can't load dialog");
+            e.printStackTrace();
+            return;
+        }
+        newTransactionController.getDialogPane().getButtonTypes().add(ButtonType.FINISH);
+        Optional<ButtonType> result = newTransactionController.showAndWait();
+        if (result.isPresent()) {
+            tableView.setItems(BlockchainData.getInstance().getTransactionLedgerFX());
+            eCoins.setText(BlockchainData.getInstance().getWalletBalanceFX());
+        }
+    }
+
+    @FXML
+    public void refresh() {
+        tableView.setItems(BlockchainData.getInstance().getTransactionLedgerFX());
+        tableView.getSelectionModel().select(0);
+        eCoins.setText(BlockchainData.getInstance().getWalletBalanceFX());
+    }
+
+    @FXML
+    public void handleExit() {
+        BlockchainData.getInstance().setExit(true);
+        Platform.exit();
+    }
+}
