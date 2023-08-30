3a14daa516f12e4327df548ffbc3edaa253685af
lumoyano <121978205+lumoyano@users.noreply.github.com>
2023-08-29 18:44:01 -0600

    Finished MiningThread

--- src/main/threads/MiningThread.java
+++ src/main/threads/MiningThread.java
@@ -1,5 +1,36 @@
 package main.threads;
 
-public class MiningThread {
-    //pp 111...
+import main.serviceData.BlockchainData;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+public class MiningThread extends Thread {
+    @Override
+    public void run() {
+        while (true) {
+            long lastMinedBlock = LocalDateTime.parse(
+                    BlockchainData.getInstance().getCurrentBlockchain.getLast().getTimestamp())
+                    .toEpochSecond(ZoneOffset.UTC);
+            if (lastMinedBlock + BlockchainData.getTimeoutInterval() < LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)) {
+                System.out.println("Blockchain is too old for mining! Update it from Peers");
+            } else if ( ((lastMinedBlock + BlockchainData.getMiningInterval()) - LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)) > 0 ) {
+                System.out.println("Blockchain is current, mining will commence in " +
+                        ((lastMinedBlock + BlockchainData.getMiningInterval()) - LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)) +
+                        " seconds");
+            } else {
+                System.out.println("MINING NEW BLOCK");
+                BlockchainData.getInstance().mineBlock();
+                System.out.println(BlockchainData.getInstance().getWalletBalanceFX());
+            }
+            System.out.println(LocalDateTime.parse(BlockchainData.getInstance().getCurrentBlockChain().getLast().getTimestamp()).toEpochSecond(ZoneOffset.UTC));
+            try {
+                Thread.sleep(2000);
+                if (BlockchainData.getInstance().isExit()) break;
+                BlockchainData.getInstance().setMiningPoints(BlockchainData.getInstance().getMiningPoints() + 2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
