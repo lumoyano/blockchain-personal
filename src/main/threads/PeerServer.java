b54e4e5969a2bbdf643bf41ae115f9d4c790a806
lumoyano <121978205+lumoyano@users.noreply.github.com>
2023-09-07 00:32:23 -0600

    Finished peer server Thread

---
+++ src/main/threads/PeerServer.java
@@ -0,0 +1,22 @@
+package main.threads;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class PeerServer extends  Thread{
+    private ServerSocket serverSocket;
+    public PeerServer (Integer socketPort) throws IOException {
+        this.serverSocket = new ServerSocket(socketPort);
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                new PeerRequestThread(serverSocket.accept()).start();
+            } catch (IOException) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
