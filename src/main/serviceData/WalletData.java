9438149c3c76f27995861afc0e4e496f469dc341
lumoyano <121978205+lumoyano@users.noreply.github.com>
2023-09-12 10:38:56 -0600

    Finished WalletData

--- src/main/serviceData/WalletData.java
+++ src/main/serviceData/WalletData.java
@@ -1,5 +1,41 @@
 package main.serviceData;
 
+import main.model.Wallet;
+
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.sql.*;
+
 public class WalletData {
-    //pp 122...
+    private Wallet wallet;
+
+    private static WalletData instance;
+
+    static {
+        instance = new WalletData();
+    }
+
+    public static WalletData getInstance() {
+        return instance;
+    }
+    public void loadWallet() throws SQLException, NoSuchAlgorithmException, InvalidKeySpecException {
+        Connection walletConnection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\lumoy\\OneDrive\\Área de Trabalho\\Workspaces\\blockchain-personal\\db\\wallet.db");
+        Statement walletStatement = walletConnection.createStatement();
+        ResultSet resultSet;
+        resultSet = walletStatement.executeQuery(" SELECT * FROM WALLET ");
+        KeyFactory keyFactory = KeyFactory.getInstance("    DSA");
+        PublicKey pub2 = null;
+        PrivateKey prv2 = null;
+        while(resultSet.next()) {
+            pub2 = keyFactory.generatePublic(new X509EncodedKeySpec(resultSet.getBytes("PUBLIC_KEY")));
+            prv2 = KeyFactory.generatePrivate(new PKCS8EncodedKeySpec(resultSet.getBytes("PRIVATE_KEY")));
+        }
+        this.wallet = new Wallet(pub2, prv2);
+    }
+
+    public Wallet getWallet() {
+        return wallet;
+    }
 }
